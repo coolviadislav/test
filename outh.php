b2f4576a99d89569836af0a3f3e8a23633afb3fe
coolviadislav <73261367+coolviadislav@users.noreply.github.com>
2020-11-12 17:50:24 +0300

    Update outh.php

--- outh.php
+++ outh.php
@@ -1,9 +1,13 @@
 <?php
 /*
+	Проверка на наличие сессии
 	Приветствие авторизованного пользователя 
 	Кнопка закрытия сессии 
 */
  session_start();
+ if (!isset($_SESSION['name'])) {
+ 	header("Location: login.html.php");
+ }
  echo $_SESSION['name'];
 ?>
 <form action="logout.php" target="_blank">
